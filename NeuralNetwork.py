667c235b28d1c7a4175fe31806616e5318761c96
Brandon Schabell <brandonschabell@gmail.com>
2018-06-22 02:24:20 -0500

    Added evolve methods

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,29 +1,90 @@
-import numpy as np
-import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense
 from keras import backend as K
 from keras.optimizers import Adam
-import matplotlib.pyplot as plt
 import random
 import math
+from copy import deepcopy
 
+ACTIVATIONS_LIST = ['softmax', 'elu', 'selu', 'softplus', 'softsign', 'relu', 
+                    'tanh', 'sigmoid', 'hard_sigmoid', 'linear']
+MUTATABLE_PARAMS = ['neuron_counts', 'activation_functions', 'layer_count']
+
+def r2_keras(y_true, y_pred):
+    SS_res =  K.sum(K.square(y_true - y_pred)) 
+    SS_tot = K.sum(K.square(y_true - K.mean(y_true))) 
+    return ( 1 - SS_res/(SS_tot + K.epsilon()) )
+
+
+def diff(y_true, y_pred):
+    return K.mean(abs(y_pred - y_true))
+
+
+class Population:
+    def __init__(self, 
+                 count,
+                 retain_percentage,
+                 input_dimension, 
+                 hidden_layer_neuron_counts, 
+                 activation_functions, 
+                 x=None,
+                 y=None,
+                 mutate_chance=0.2,
+                 batch_size=None,
+                 epochs=1,
+                 verbose=1,
+                 validation_split=0.3, 
+                 learning_rate=0.001, 
+                 kernel_initializer='normal', 
+                 loss_function='mean_squared_error', 
+                 metrics=[r2_keras, diff]):
+        self.pop = []
+        self.retain_percentage = retain_percentage
+        self.mutate_chance = mutate_chance
+        for i in range(count):
+            nn = NeuralNetwork(input_dimension,
+                               hidden_layer_neuron_counts, 
+                               activation_functions, 
+                               x,
+                               y,
+                               batch_size,
+                               epochs,
+                               verbose,
+                               validation_split, 
+                               learning_rate,
+                               kernel_initializer, 
+                               loss_function, 
+                               metrics)
+            if i > 0:
+                while random.random() > 0.2:
+                    nn.mutate()
+            self.pop.append(nn)
+    
+    
+    def evolve(self):
+        graded = [(nn.get_score(), nn) for nn in self.pop]
+        graded = [x[1] for x in sorted(graded, key=lambda x: x[0], reverse=False)]
+        retained_length = int(len(graded) * self.retain_percentage)
+        keep = graded[:retained_length]
+        for nn in keep:
+            if self.mutate_chance > random.random():
+                new_nn = deepcopy(nn)
+                keep.append(new_nn.mutate())
+            
+        while len(keep) < len(self.pop):
+            new_nn = deepcopy(keep[0])
+            new_nn.mutate()
+            while random.random() > 0.4:
+                new_nn.mutate()
+            keep.append(new_nn)
+        self.pop = keep
+
+    
+    def get_top_score(self):
+        return self.pop[0].get_score()
+    
 
 class NeuralNetwork:
-    mutatable_params = ['neuron_counts', 'activation_functions', 'layer_count']
-    activations_list = ['softmax', 'elu', 'selu', 'softplus', 'softsign', 
-                        'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear']
-    
-    def r2_keras(y_true, y_pred):
-        SS_res =  K.sum(K.square(y_true - y_pred)) 
-        SS_tot = K.sum(K.square(y_true - K.mean(y_true))) 
-        return ( 1 - SS_res/(SS_tot + K.epsilon()) )
-
-
-    def diff(y_true, y_pred):
-        return K.mean(abs(y_pred - y_true))
-    
-    
     def __init__(self, 
                  input_dimension, 
                  hidden_layer_neuron_counts, 
@@ -33,7 +94,7 @@ class NeuralNetwork:
                  batch_size=None,
                  epochs=1,
                  verbose=1,
-                 validation_split=0., 
+                 validation_split=0.3, 
                  learning_rate=0.001, 
                  kernel_initializer='normal', 
                  loss_function='mean_squared_error', 
@@ -55,10 +116,32 @@ class NeuralNetwork:
         self.is_processed = False
     
     
+    def __deepcopy__(self, memo):
+        cls = self.__class__
+        result = cls.__new__(cls)
+        memo[id(self)] = result
+        result.input_dimension = self.input_dimension
+        result.hidden_layer_neuron_counts = self.hidden_layer_neuron_counts
+        result.activation_functions = self.activation_functions
+        result.x = self.x
+        result.y = self.y
+        result.batch_size = self.batch_size
+        result.epochs = self.epochs
+        result.verbose = self.verbose
+        result.validation_split = self.validation_split
+        result.learning_rate = self.learning_rate
+        result.kernel_initializer = self.kernel_initializer
+        result.loss_function = self.loss_function
+        result.metrics = self.metrics
+        result.model = Sequential()
+        result.is_processed = False
+        return result
+    
+    
     def mutate(self):
         self.model = Sequential()
         self.is_processed = False
-        param = random.choice(self.mutatable_params)
+        param = random.choice(MUTATABLE_PARAMS)
         if param == 'neuron_counts':
             if len(self.hidden_layer_neuron_counts) == 0:
                 return self.mutate()
@@ -67,17 +150,27 @@ class NeuralNetwork:
             if random.random() > 0.5:
                 self.hidden_layer_neuron_counts[index] = cur_val + 1
             else:
+                if cur_val == 1:
+                    return self.mutate()
                 self.hidden_layer_neuron_counts[index] = cur_val - 1
         elif param == 'activation_functions':
-            index = random.randint(0, len(self.activation_functions) - 1)
-            new_val = random.choice(self.activations_list)
-            self.activation_functions[index] = new_val
+            index = random.randint(0, len(ACTIVATIONS_LIST) - 1)
+            new_val = random.choice(ACTIVATIONS_LIST)
+            ACTIVATIONS_LIST[index] = new_val
         elif param == 'layer_count':
-            new_neuron_count = random.randint(
-                    math.ceil(self.input_dimension * 0.5),
-                    self.input_dimension * 2)
-            self.hidden_layer_neuron_counts.insert(0, new_neuron_count)
-            self.activation_functions.insert(1, self.activation_functions[0])
+            if random.random() > 0.5:
+                new_neuron_count = random.randint(
+                        math.ceil(self.input_dimension * 0.5),
+                        self.input_dimension * 2)
+                self.hidden_layer_neuron_counts.insert(0, new_neuron_count)
+                self.activation_functions.insert(1, ACTIVATIONS_LIST[0])
+            elif len(self.hidden_layer_neuron_counts) > 0:
+                index = random.randint(0, len(self.hidden_layer_neuron_counts) - 1)
+                del self.hidden_layer_neuron_counts[index]
+                del self.activation_functions[index + 1]
+            else:
+                return self.mutate()
+        return self
     
     
     def process_model(self):
--- test.py
+++ test.py
@@ -1,38 +1,49 @@
-from NeuralNetwork import NeuralNetwork
+import NeuralNetwork
 import numpy as np
 import pandas as pd
 import random
 
 
-x = np.array(list(range(1, 10000)))
-y = np.linspace(47, 4747, 9999)
+x = np.array(list(range(1, 1000)))
+y = np.linspace(47, 4747, 999)
 z = (x + y) + random.gauss(0, 0.2)
 df = pd.DataFrame({'x': x, 'y': y, 'z': z})
 
-EPOCH_COUNT = 20
+EPOCH_COUNT = 50
 
-nn = NeuralNetwork(2, [2], ['linear', 'linear'], df[['x', 'y']], df[['z']], 32, EPOCH_COUNT, 1, 0.3, 0.001)
-diff = nn.get_score()
+#nn = NeuralNetwork.NeuralNetwork(2, [2], ['linear', 'linear'], df[['x', 'y']], df[['z']], 32, EPOCH_COUNT, 1, 0.3, 0.001)
+#diff = nn.get_score()
 
-print('diff={}'.format(diff))
+#print("Initial model summary:")
+#print(model.summary())
+#
+#print("Mutating...")
+#nn.mutate()
+#model_mutated = nn.get_model()
+#print("New model summary:")
+#print(model_mutated.summary())
 
-test = pd.DataFrame({'x': [9999], 'y': [4747]})
-model = nn.get_model()
-model.predict(test)
+print("Creating population")
+population = NeuralNetwork.Population(count=100,
+                 retain_percentage=0.7,
+                 mutate_chance=0.2,
+                 input_dimension=2, 
+                 hidden_layer_neuron_counts=[2], 
+                 activation_functions=['linear', 'linear'], 
+                 x=df[['x', 'y']],
+                 y=df[['z']],
+                 batch_size=32,
+                 epochs=EPOCH_COUNT,
+                 verbose=0)
 
-def predict_value(row):
-    f = row[['x', 'y']]
-    f = np.expand_dims(f, axis=0)
-    prediction = model.predict(f)[0][0]
-    return prediction
+print("Evolving population...")
+top_scores = []
+for i in range(50):
+    print('Starting round {}...'.format(i + 1))
+    population.evolve()
+    top_score = population.get_top_score()
+    top_scores.append(top_score)
+    print('Top score = {}'.format(top_score))
 
-df.loc[:,'pred'] = df.apply(lambda row: predict_value(row), axis=1)
-
-print("Initial model summary:")
-print(model.summary())
-
-print("Mutating...")
-nn.mutate()
-model_mutated = nn.get_model()
-print("New model summary:")
-print(model_mutated.summary())
+print("Done.")
+top_nn = population.pop[0]
